ea52fd2bc1f9741a048419b6adf1413f8d071049
Pinkmann <korgov.vova@icloud.com>
2021-04-23 20:52:25 +0300

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,58 @@
+const mix         = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+
+/**
+ * Configure webpack.
+ */
+mix.webpackConfig({
+    resolve: {
+        extensions: [ '.js', '.vue' ],
+        alias: {
+            '@': `${__dirname}/resources/js`
+        }
+    }
+})
+
+/**
+ * Configure options.
+ */
+mix.options({
+    processCssUrls: false,
+    postCss: [
+        tailwindcss({
+            prefix: 'mpcfq\:',
+            purge: [
+                'public/*.html', 'resources/js/*.js',
+            ],
+            variants: {
+                borderWidth: [ 'responsive', 'last' ],
+                cursor: [ 'responsive', 'hover' ]
+            },
+            theme: {
+                extend: {
+                    screens: {
+                        'lg': '951px',
+                        'xl': '1141px'
+                    },
+                    padding: {
+                        '3.5': '0.875rem'
+                    },
+                    margin: {
+                        '1.5': '0.375rem',
+                        '-3.5': '-0.875rem'
+                    }
+                }
+            }
+        })
+    ]
+});
+
+/**
+ * Compile JS.
+ */
+mix.js('./resources/js/frontend.js', './public/js');
+
+/**
+ * Compile SASS.
+ */
+mix.sass('./resources/sass/frontend.sass', './public/css');
